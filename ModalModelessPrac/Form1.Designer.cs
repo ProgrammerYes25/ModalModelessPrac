47bc42735adb25fad9fec81e7e88cf288fb54ec7
yes25 <programmer.yes25@gmail.com>
2023-06-12 15:13:21 +0900

    다양한 메시지박스 옵션 실습

--- ModalModelessPrac/Form1.Designer.cs
+++ ModalModelessPrac/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace ModalModelessPrac
+﻿namespace ModalModelessPrac31
 {
     partial class Form1
     {
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(54, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 82);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(418, 458);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
-
--- ModalModelessPrac/Form1.cs
+++ ModalModelessPrac/Form1.cs
@@ -8,13 +8,29 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ModalModelessPrac
+namespace ModalModelessPrac31
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("내용");
+            MessageBox.Show("내용", "제목");
+            DialogResult result;
+            do
+            {
+                result = MessageBox.Show("내용", "제목", MessageBoxButtons.RetryCancel);
+            } while (result == DialogResult.Retry);
+            if (result == DialogResult.Cancel)
+            {
+                MessageBox.Show("사용자가 취소하였습니다.", "알림",
+                    MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
         }
     }
 }
